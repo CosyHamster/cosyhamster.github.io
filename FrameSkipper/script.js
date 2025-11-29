1874cd4da759c013025c689601bfa6e9b87b208d
matth <cosyhamster@gmail.com>
2025-11-28 22:03:43 -0500

    clean up landing page for the site clean up frame skipper

--- FrameSkipper/script.js
+++ FrameSkipper/script.js
@@ -4,53 +4,58 @@ const fileUpload = document.getElementById("upload");
 const skipForwardButton = document.getElementById("skipForward");
 const skipBackwardButton = document.getElementById("skipBackward");
 const skipDuration = document.getElementById("skipDuration");
-const currentTimeInput = document.getElementById("currentTimeInput");
-const videoFramerateInput = document.getElementById("videoFrameRate");
+/** @type HTMLInputElement */
+const inputMediaTime = document.getElementById("mediaTimeInput");
+/** @type HTMLInputElement */
+const inputFrameRate = document.getElementById("videoFrameRate");
 const calcFramerateButton = document.getElementById("calcFramerate");
-const calcFrameButton = document.getElementById("calcCurrentVideoFrame");
+const calcFrameNumberButton = document.getElementById("calcFrameNumber");
+/** @type HTMLOutputElement */
 const outputFrame = document.getElementById("outputFrame");
+/** @type HTMLVideoElement */
 const video = document.getElementById("video");
 var seeking = 0;
 var iters = 0;
 var currentMediaTime = 0;
-/** @type null | (framerate: string) => null */
+var frameRate = NaN;
+/** @type null | (framerate: number) => null */
 let resolveWithVideoFrameRate = null;
 
-
+setButtonsDisabled(true);
 
 function uploadFile(file){
-	URL.revokeObjectURL(video.src);
+	if(video.src)
+		URL.revokeObjectURL(video.src);
+	else
+		setButtonsDisabled(false);
 	video.src = URL.createObjectURL(file);
 	video.play();
 }
 fileUpload.addEventListener("change", () => {
 	uploadFile(fileUpload.files[0]);
-});
+}, true);
 document.addEventListener("dragover", (event) => {
 	const dataTransfer = event.dataTransfer;
-	if(dataTransfer.types.length === 1 && dataTransfer.types.includes("Files"))
+	if(dataTransfer.types.includes("Files"))
 		event.preventDefault();
 });
 document.addEventListener("drop", (event) => {
 	const dataTransfer = event.dataTransfer;
-	if(dataTransfer.types.length === 1 && dataTransfer.types.includes("Files")){
+	if(dataTransfer.types.includes("Files")){
 		event.preventDefault();
 		uploadFile(dataTransfer.files[0]);
 	}
 });
-
-
 
 function videoFrameAdvanced(now, metadata){
 	console.log(`iters: ${iters}; currentTime: ${video.currentTime}; prevMediaTime: ${currentMediaTime}; mediaTime: ${metadata.mediaTime}`);
 	skipDuration.valueAsNumber = (skipDuration.valueAsNumber*iters)/PREFERRED_ITERS;
 	if(iters >= PREFERRED_ITERS){
 		if(resolveWithVideoFrameRate != null){
-			console.log((1/(metadata.mediaTime-currentMediaTime)))
 			resolveWithVideoFrameRate(1/(metadata.mediaTime-currentMediaTime));
 		}
-		currentMediaTime = metadata.mediaTime+0.0001;
-		currentTimeInput.valueAsNumber = metadata.mediaTime;
+		currentMediaTime = metadata.mediaTime;
+		inputMediaTime.valueAsNumber = metadata.mediaTime;
 		setButtonsDisabled(false);
 		seeking = 0;
 		iters = 0;
@@ -67,13 +72,12 @@ function onVideoFrame(now, metadata){
 	switch(seeking){
 		case -1:
 			if(resolveWithVideoFrameRate != null){
-				console.log((1/(currentMediaTime-metadata.mediaTime)))
 				resolveWithVideoFrameRate(1/(currentMediaTime-metadata.mediaTime));
 			}
 			seeking = 0;
 		case 0:
-			currentMediaTime = metadata.mediaTime+0.0001;
-			currentTimeInput.valueAsNumber = currentMediaTime;
+			currentMediaTime = metadata.mediaTime;
+			inputMediaTime.valueAsNumber = currentMediaTime;
 			break;
 		case 1:
 			if(currentMediaTime < metadata.mediaTime){
@@ -90,51 +94,81 @@ video.requestVideoFrameCallback(onVideoFrame);
 
 function skipBackward(){
 	seeking = -1;
-	video.currentTime = currentMediaTime-0.0002;
+	video.currentTime = currentMediaTime-0.0001;
 }
 function skipForward(){
 	seeking = 1;
-	if(video.currentTime === currentMediaTime)
-		video.currentTime = currentMediaTime+0.0001;
+	//change video time to be super slightly above mediaTime (to reduce precision errors) and ensure it is different from the currentTime so the video is forced to seek
+	if(video.currentTime === currentMediaTime+0.0001)
+		video.currentTime = currentMediaTime+0.0002;
 	else
-		video.currentTime = currentMediaTime;
+		video.currentTime = currentMediaTime+0.0001;
 }
-
-skipBackwardButton.addEventListener("click", () => {
-	setButtonsDisabled(true);
-	video.addEventListener("seeked", () => {
-		setButtonsDisabled(false);
-	}, {once: true});
-	skipBackward();
-});
-skipForwardButton.addEventListener("click", () => {
-	setButtonsDisabled(true);
-	skipForward();
-});
-calcFramerateButton.addEventListener("click", async () => {
-	setButtonsDisabled(true);
-	const backwardFramerate = await new Promise(resolve => {
-		resolveWithVideoFrameRate = resolve;
-		skipBackward();
-	});
-	const forwardFramerate = await new Promise(resolve => {
-		resolveWithVideoFrameRate = resolve;
-		skipForward();
-	});
-	videoFramerateInput.value = ((backwardFramerate+forwardFramerate)/2).toFixed(2);
-}, true);
-calcFrameButton.addEventListener("click", () => {
-	const frameRate = videoFramerateInput.valueAsNumber;
-	if(!frameRate)
+function calcFrameNumberAndDisplay(){
+	if(!frameRate){
+		inputFrameRate.focus();
 		return;
-	outputFrame.innerText = currentMediaTime*frameRate;
-}, true);
-currentTimeInput.addEventListener("change", () => {
-	video.currentTime = currentTimeInput.valueAsNumber;
-}, true);
-
+	}
+	const frameNumber = currentMediaTime*frameRate;
+	console.log("RAW: " + String(frameNumber))
+	outputFrame.innerText = String(Math.round(frameNumber));
+}
 function setButtonsDisabled(disabled){
 	skipForwardButton.disabled = disabled;
 	skipBackwardButton.disabled = disabled;
 	calcFramerateButton.disabled = disabled;
+	video.inert = disabled;
 }
+
+
+skipBackwardButton.addEventListener("click", () => {
+	setButtonsDisabled(true);
+	video.pause();
+	video.addEventListener("seeked", () => {
+		setButtonsDisabled(false);
+	}, {once: true});
+	skipBackward();
+}, true);
+
+skipForwardButton.addEventListener("click", () => {
+	if(video.ended) return;
+	video.pause();
+	setButtonsDisabled(true);
+	skipForward();
+}, true);
+
+calcFramerateButton.addEventListener("click", async () => {
+	if(currentMediaTime <= 0.0001 || video.ended) return;
+	setButtonsDisabled(true);
+	video.pause();
+	const backwardFramerate = await new Promise(resolve => {
+		resolveWithVideoFrameRate = resolve;
+		skipBackward();
+	});
+	console.log("Frame rate backward: " + String(backwardFramerate));
+	await new Promise(resolve => setTimeout(resolve, 0)); //allow normal processes to finish before starting next thingy
+	const forwardFramerate = await new Promise(resolve => {
+		resolveWithVideoFrameRate = resolve;
+		skipForward();
+	});
+	console.log("Frame rate forward: " + String(forwardFramerate));
+
+	// frameRate = ((backwardFramerate+forwardFramerate)/2);
+	frameRate = (backwardFramerate+forwardFramerate)/2;
+	console.log("Framerate raw: " + String(frameRate));
+	frameRate = Math.round(frameRate*100)/100;
+	console.log("Framerate fixed: " + String(frameRate));
+	inputFrameRate.value = frameRate;//.toFixed(2);
+}, true);
+
+inputFrameRate.addEventListener("change", () => {
+	frameRate = inputFrameRate.valueAsNumber;
+})
+
+calcFrameNumberButton.addEventListener("click", () => {
+	calcFrameNumberAndDisplay();
+}, true);
+
+inputMediaTime.addEventListener("change", () => {
+	video.currentTime = inputMediaTime.valueAsNumber;
+}, true);
